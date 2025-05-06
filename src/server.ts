748094414d120341ae9640b319aa0f43965811da
Leonardo Coelho <leoocoelhoow06@gmail.com>
2025-05-05 23:44:43 -0300

    Route Params

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,10 @@ const PORT = 3333
 
 const app = express()
 
-app.get("/", (request, response) => {
-  response.send("Hello World Express!")
+app.get("/products/:id/:user", (request, response) => { // o Nome que se dá dps dos ":" vc consegue recuperar direto de request.params
+  const {id, user} = request.params
+
+  response.send(`${id}, ${user}`)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
