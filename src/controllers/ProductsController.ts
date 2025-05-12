253f6907c3ea03fd4284e9e3262159465385548d
Leonardo Coelho <leoocoelhoow06@gmail.com>
2025-05-12 19:52:06 -0300

    Controller

---
+++ src/controllers/ProductsController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express"
+
+class ProductsController {
+  /** Um controller tem que receber até no máximo 5 métodos, mais que isso é viável outro controller
+   * index - GET para listar
+   * show - GET para exibir um registro específico
+   * create - POST para criar registro
+   * update - PUT para atualizar
+   * remove - DELETE para remover um registro
+  */
+
+  index(request: Request, response: Response) {
+    //  /products?page=1&limit=10
+    const {page, limit} = request.query
+    
+    response.send(`Página ${page} de ${limit}`)
+  }
+
+  create(request: Request, response: Response){
+    const {name, price} = request.body // const recebendo os mesmos nomes do body no insomnia
+
+    // response.send(`Produto ${name}, preço ${price}`)
+    response.status(201).json({name, price, user_id: request.user_id})
+  }
+
+
+
+}
+
+export { ProductsController }
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -1,20 +1,12 @@
 import { Router } from "express"
 import { myMiddleware } from "../middlewares/my-middleware"
+import { ProductsController } from "../controllers/ProductsController"
 
 const productsRoutes = Router()
+const productsController = new ProductsController()
 
-productsRoutes.get("/:id", (request, response) => {
-  //  /products?page=1&limit=10
-  const {page, limit} = request.query
+productsRoutes.get("/", productsController.index)
 
-  response.send(`Página ${page} de ${limit}`)
-})
-
-productsRoutes.post("/", myMiddleware, (request, response) => { // Uso local do middleware
-  const {name, price} = request.body // const recebendo os mesmos nomes do body no insomnia
-
-  // response.send(`Produto ${name}, preço ${price}`)
-  response.status(201).json({name, price, user_id: request.user_id})
-})
+productsRoutes.post("/", myMiddleware, productsController.create)
 
 export { productsRoutes }
